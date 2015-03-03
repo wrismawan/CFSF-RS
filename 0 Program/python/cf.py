626eae2ef7ab0380cb40727403cbfd45638cbfa1
Wahyu Rismawan <wahyurismawan@gmail.com>
2015-03-03 19:21:29 +0700

    iCluster :D

--- 0 Program/python/cf.py
+++ 0 Program/python/cf.py
@@ -4,15 +4,19 @@ import peewee as pw
 from math import sqrt
 from random import randint
 from kmeans import *
+import collections
 
+i_cluster = collections.namedtuple("iCluster","sim num_cluster")
 class CF(object):
 
-	def __init__(self, max_user, max_movie):
+	def __init__(self, max_user, max_movie, k):
+		self.k = k
 		self.max_user = max_user
 		self.max_movie = max_movie
 		self.item_user = [[0 for x in range(max_user)] for x in range(max_movie)]
 		self.user_item = [[u if m==0 else 0 for m in range(max_movie)] for u in range(max_user)]
 		self.matrix_GIS = [[0 for x in range(self.max_movie)] for x in range(self.max_movie)]
+		self.iCluster = [[i_cluster(-1, c) for c in range(self.k)] for u in range(self.max_user)]
 		
 		rating_query = Rating.select().where(Rating.user_id < max_user).where(Rating.movie_id < max_movie).order_by(Rating.movie_id).limit(max_movie)
 		for r in rating_query:
@@ -58,7 +62,7 @@ class CF(object):
 
 	def clusterUser(self, user_item):
 		data_user_item = self.writeToCSV(listData=user_item,fileName='output/user_item.csv')
-		cluster = kClusterer(data_user_item, 2)
+		cluster = kClusterer(data_user_item, self.k)
 		cluster.kCluster()
 		user_cluster = cluster.listCluster()
 		self.writeToCSV(listData=user_cluster, fileName='output/user_cluster.csv')
@@ -71,6 +75,18 @@ class CF(object):
 				return c
 			c += 1
 
+	def average_rating_user(self,u):
+		return float(sum(self.user_item[u]) - sum([x for x in range(self.max_user)]))  / self.max_movie
+
+	def calc_RCui(self, num_cluster,item):
+		RCui = 0
+		len_cluster = len(self.user_cluster[num_cluster])
+		for u in self.user_cluster[num_cluster]:
+			RCui += (self.user_item[int(u)][item] - self.average_rating_user(int(u))) / len_cluster
+		return RCui
+
+
+
 	def run(self):
 
 		#1 Create GIS
@@ -79,25 +95,32 @@ class CF(object):
 		#2 Cluster User
 		self.user_cluster = self.clusterUser(self.user_item)
 
-		print self.user_cluster
 		# 3 Smoothing
 		for user in range(1,len(self.user_item)):
 			selected_cluster = self.getUserCluster(user)
 			for item in range(len(self.user_item[user])):
 				if (self.user_item[user][item] == 0):
-					_sum = 0
-					for u in self.user_cluster[selected_cluster]:
-						average_rating_u =  float(sum(self.user_item[int(u)]) - sum([x for x in range(self.max_user)]))  / self.max_movie
-						_sum += self.user_item[int(u)][item] - average_rating_u
-
-					RC = _sum/len(self.user_cluster[selected_cluster])
-					self.user_item[user][item] = RC
+					self.user_item[user][item] = self.calc_RCui(num_cluster=selected_cluster, item=item)
 
 		#4 Create iCluster
-		
+		for user in range(self.max_user):
+			av_rating = self.average_rating_user(user)
+			for cluster in range(self.k):
+				pembilang = 0
+				for item in range(self.max_movie):
+					pembilang +=  self.calc_RCui(cluster, item) * (self.user_item[user][item] - av_rating)
+					sum_rcui = self.calc_RCui(cluster, item) ** 2 
+					sum_rui = (self.user_item[user][item] - av_rating) **2
+				sim = pembilang / ((math.sqrt(sum_rcui) * math.sqrt(sum_rui)) + .0000001)
+				self.iCluster[user][cluster] = self.iCluster[user][cluster]._replace(sim = sim)
+			print sorted(self.iCluster[user],reverse=True)
+
+
+
+
 
 if __name__ == "__main__":
-	cf = CF(5,5)
+	cf = CF(max_user=5,max_movie=5,k=3)
 	cf.run()
 
 	cf.print_matrix("USER-ITEM", cf.user_item)
